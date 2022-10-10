507c92585a54b0a5ede9980f92b92a97fab85744
NittaDragon <uspemskaya@gmail.com>
2022-10-10 15:04:07 +0300

    ad2

--- saod1.1.cpp
+++ saod1.1.cpp
@@ -30,22 +30,70 @@ int main()
       cout<<endl;
     }
   }
-    for (i=0;i<m;i++)
+  for (i=0;i<m;i++) // сортировка выбором
+  {
+    int min=m+1;
+    for (j=i;j<m;j++)
     {
-      int min=m+1;
-      for (j=i;j<m;j++)
+      if(a[j]<min)
       {
-        if(a[j]<min)
-        {
-          min=a[j];
-          t=j;
-        }
-      }
-      if(a[i]!=min)
-      {
-        k=a[i];
-        a[i]=min;
-        a[t]=k;
+        min=a[j];
+        t=j;
       }
     }
+    if(a[i]!=min)
+    {
+      k=a[i];
+      a[i]=min;
+      a[t]=k;
+    }
+  }
+  for (i=1;i<m;i++) //сортировка вставками
+  {
+    for (j=0;j<i;j++)
+    {
+      if(a[i]<a[j])
+      {
+        k=a[i];
+        a[i]=a[j];
+        a[j]=k;
+      }
+    }
+    for(int s=0;s<m;s++)
+    {
+      cout<<a[s]<<", ";
+    }
+    cout<<endl;
+  }
+  /*k=1;
+  t=0;
+  for(int s=0;s<m;s++)
+  {
+    cout<<a[s]<<", ";
+  }
+  cout<<endl;
+  while(t<(m-1)) // сортировка слиянием
+  {
+    int n=k-1;
+    k=k*2;
+
+    for(i=0;i<m;i+=k)
+    {
+      /*for(j=1;j<m;j+=k)
+      {*/
+      /*  if(a[i]>a[i+1])
+        {
+          l=a[i];
+          a[i]=a[i+1];
+          a[i+1]=l;
+        }
+      //}
+    }
+    for(int s=0;s<m;s++)
+    {
+      cout<<a[s]<<", ";
+    }
+    cout<<endl;
+    t++;
+  }*/
 }
